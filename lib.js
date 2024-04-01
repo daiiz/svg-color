51d6f6f09057523e37d4cde1bd94fd2e151b6417
daiiz <da.iz.mg@gmail.com>
2024-04-01 17:46:58 +0900

    supprt circle

--- lib.js
+++ lib.js
@@ -1,10 +1,21 @@
 const createSvgStr = ({ color, shape }) => {
-  console.log("color", color, "shape", shape);
   const width = 20;
   const height = 20;
-  return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${width} ${height}" width="${width}" height="${height}">
-      <rect x="0" y="0" width="${width}" height="${height}" fill="${color}"></rect>
-    </svg>`;
+
+  const figureElementStr = (function () {
+    if (shape === "circle") {
+      const cx = width / 2;
+      const cy = height / 2;
+      return `<circle cx="${cx}" cy="${cy}" r="${cx}" fill="${color}"></circle>`;
+    }
+    return `<rect x="0" y="0" width="${width}" height="${height}" fill="${color}"></rect>`;
+  })();
+
+  return [
+    `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ${width} ${height}" width="${width}" height="${height}">`,
+    figureElementStr,
+    "</svg>",
+  ].join("");
 };
 
 const createResponseHeader = ({ color }) => {
