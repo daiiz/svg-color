5dee62e79b4c98c4d238b2e4ad65faae315ba822
daiiz <da.iz.mg@gmail.com>
2024-04-01 17:35:38 +0900

    impl parseOptions

--- src/hex.js
+++ src/hex.js
@@ -1,9 +1,25 @@
-const { createSvgStr, createResponseHeader } = require('../lib')
+const { createSvgStr, createResponseHeader } = require("../lib");
+
+function parseOptions(optionsStr = "") {
+  const toks = optionsStr
+    .split(".")
+    .map((tok) => tok.trim())
+    .filter((tok) => !!tok);
+  const options = Object.create(null);
+  for (const tok of toks) {
+    if (["circle"].includes(tok)) {
+      options.shape = tok;
+    }
+  }
+  return options;
+}
 
 module.exports = async (req, res) => {
-  const { hex } = req.query
-  const color = `#${hex}`
-  const svgStr = createSvgStr({ color })
-  res.writeHead(200, createResponseHeader({ color }))
-  res.end(svgStr)
-}
+  const { hex } = req.query;
+  const options = parseOptions(req.query.options);
+  console.log(`hex: ${hex}`, options);
+  const color = `#${hex}`;
+  const svgStr = createSvgStr({ color });
+  res.writeHead(200, createResponseHeader({ color }));
+  res.end(svgStr);
+};
