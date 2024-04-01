688f3a5bdbcfdbb36231cb92fbae292f5abcf19c
daiiz <da.iz.mg@gmail.com>
2024-04-01 18:20:14 +0900

    rgba

--- src/rgba.js
+++ src/rgba.js
@@ -1,16 +1,17 @@
-const { createSvgStr, createResponseHeader } = require('../lib')
+const { createSvgStr, createResponseHeader, parseOptions } = require("../lib");
 
 module.exports = async (req, res) => {
-  const { r, g, b, a } = req.query
-  let colorStr = `${parseInt(r)},${parseInt(g)},${parseInt(b)}`
-  let color = `rgb(${colorStr})`
+  const { r, g, b, a } = req.query;
+  let colorStr = `${parseInt(r)},${parseInt(g)},${parseInt(b)}`;
+  let color = `rgb(${colorStr})`;
 
   if (a !== undefined) {
-    colorStr = `${colorStr},${Math.min(1, parseFloat(a))}`
-    color = `rgba(${colorStr})`
+    colorStr = `${colorStr},${Math.min(1, parseFloat(a))}`;
+    color = `rgba(${colorStr})`;
   }
 
-  const svgStr = createSvgStr({ color })
-  res.writeHead(200, createResponseHeader({ color }))
-  res.end(svgStr)
-}
+  const options = parseOptions(req.query.options);
+  const svgStr = createSvgStr({ ...options, color });
+  res.writeHead(200, createResponseHeader({ color }));
+  res.end(svgStr);
+};
